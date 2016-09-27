677014e087df2ddde5ada5512ad26178743774cb
walts18 <walts.18.00@gmail.com>
2016-09-27 00:13:17 -0400

    Add files via upload

---
+++ perro.js
@@ -0,0 +1,24 @@
+function jugar(){
+	var juguete = prompt("Indica el juguete preferido de tu perro: ");
+	alert ("El perro juega con " + juguete);
+
+}	
+
+function evacuar(){
+	var tiempo = prompt("Indica la hora de evacuar de tu perro: ");
+	var lugar = prompt("Indica el lugar preferido de tu perro para evacuar: ");
+	alert ("El perro evacua a las " + tiempo + " en " + lugar);
+}
+
+function comer(){
+var cantidad = prompt("Indica la catidad de comida que le sirves a tu perro: ");
+	var comida = prompt("Indica la comida preferida de tu perro: ");
+	var utensilio = prompt("Indica donde le sirves la comida a tu perro: ");
+	alert ("El perro le gusta comer " + cantidad + ", su comida prerida es " + comida + " y se la sirven en " + utensilio);
+		}
+		
+		
+
+
+
+
